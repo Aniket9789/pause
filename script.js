e0a1dd610c234d442b02c9904a5f5c7d2dd7b586
CRP-REDWOLF <info@redwolfhitech.com>
2024-01-18 17:50:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+    document.getElementsByClassName("fa")[3] .addEventListener("click",function() {
+        document.getElementsByClassName("links")[0].classList.toggle("showmylinks")
+
+    });
+
+</script>
